2bf669c437e15e1f87d08e17e1ec0aedf79d1980
duchessacid <katherineeeee@yandex.ru>
2019-05-06 09:27:51 +0300

    Codestyle fixes

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,11 +1,10 @@
 #include <string>
 #include <iostream>
 
-using namespace std;
 
 int main() {
-string name;
-cin >> name;
-cout << "Hello world from " << name;
+std::string name;
+std::cin >> name;
+std::cout << "Hello world from " << name;
 return 0;
 }
